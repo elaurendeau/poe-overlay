3338fdd804781fdf0ea81c240bf9db425edd4500
Elliot Laurendeau <laurendeau.elliot@gmail.com>
2022-07-02 00:51:57 -0400

    electron working

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
 });
+configureWebpack: (config) => {
+  config.devtool = "source-map";
+};
